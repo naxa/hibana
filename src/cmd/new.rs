83af78727ace5e9c06e4380e4924ac9ae792fcc6
k-nasa <htilcs1115@gmail.com>
2018-12-22 13:46:10 +0900

    Fix cmd_new to use failure

--- src/cmd/new.rs
+++ src/cmd/new.rs
@@ -1,22 +1,24 @@
 use clap::ArgMatches;
+use failure::{Error, Fail};
 use std::fs::{create_dir, File};
 use std::path::Path;
 
-pub fn cmd_new(matches: &ArgMatches) -> Result<(), String> {
+#[derive(Debug, Fail)]
+#[fail(display = "Project exists!")]
+struct ProjectExists;
+
+pub fn cmd_new(matches: &ArgMatches) -> Result<(), Error> {
     let name = matches.value_of("name").unwrap();
     let path = Path::new(name);
 
     if path.exists() {
-        return Err("project exists!".to_string());
+        return Err(Error::from(ProjectExists));
     }
 
-    match create_project_dir(path) {
-        Ok(_) => Ok(()),
-        Err(e) => Err(e.to_string()),
-    }
+    create_project_dir(path)
 }
 
-fn create_project_dir(path: &Path) -> Result<(), std::io::Error> {
+fn create_project_dir(path: &Path) -> Result<(), Error> {
     create_dir(path)?;
     create_dir(path.join("contents"))?;
     create_dir(path.join("layouts"))?;
