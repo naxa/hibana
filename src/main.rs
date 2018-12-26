22a3408c1d0eb8b5a07af3f81ca61323b113fb0d
k-nasa <htilcs1115@gmail.com>
2018-12-26 22:52:26 +0900

    Add call build subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use failure::Error;
-
 #[macro_use]
 extern crate clap;
 
@@ -7,12 +5,14 @@ mod cmd;
 
 use crate::cmd::*;
 use colored::*;
+use failure::Error;
 
 fn main() {
     let matches = build_app().get_matches();
 
     match matches.subcommand() {
         ("new", Some(matches)) => cmd_new(&matches).unwrap_or_else(|e| print_error(e)),
+        ("build", Some(_)) => cmd_build().unwrap_or_else(|e| print_error(e)),
         _ => build_app().print_help().expect("faild print help"),
     }
 }
