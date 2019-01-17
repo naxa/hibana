2ef97910044f863fc181b8e9a4548d90acf4a3e5
Yuki Okushi <huyuumi.dev@gmail.com>
2019-01-17 16:21:07 +0900

    feat: remove rocket func

--- src/cmd/serve.rs
+++ src/cmd/serve.rs
@@ -4,9 +4,6 @@ extern crate rocket_contrib;
 use rocket_contrib::serve::StaticFiles;
 
 pub fn cmd_serve() {
-    rocket().launch();
-}
-
-fn rocket() -> rocket::Rocket {
     rocket::ignite().mount("/", StaticFiles::from("public"))
+        .launch();
 }
