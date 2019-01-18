4ae12aea5a99fee56eebf508e197a5f8ec6f9a7c
Yuki Okushi <huyuumi.dev@gmail.com>
2019-01-18 15:23:12 +0900

    feat: Build files before run cmd_serve

--- src/cmd/serve.rs
+++ src/cmd/serve.rs
@@ -1,9 +1,15 @@
 extern crate rocket;
 extern crate rocket_contrib;
 
+use crate::build::cmd_build;
+use failure::Error;
 use rocket_contrib::serve::StaticFiles;
 
-pub fn cmd_serve() {
+pub fn cmd_serve() -> Result<(), Error> {
+    cmd_build()?;
+
     rocket::ignite().mount("/", StaticFiles::from("public"))
         .launch();
+
+    Ok(())
 }
